1e3d9d32e53f8267ec2ef31aa481e55beca63453
Talha Malik <talhamalik25.tm@gmail.com>
2024-12-18 19:21:02 +0500

    updated AuthUser permissions

--- account/permissions.py
+++ account/permissions.py
@@ -6,12 +6,13 @@ class AuthenticatedUserPermission(BasePermission):
         if request.user.is_anonymous:
             return False
         if view.basename in ["user"]:
-            return bool(request.user and request.user.is_authenticated)
+            if request.user and request.user.is_authenticated:
+                return bool((request.user == obj) or request.user.is_staff or request.user.is_superuser)
         return False
 
     def has_permission(self, request, view):
         if view.basename in ["user"]:
             if request.user.is_anonymous:
                 return False
-            return bool(request.user and request.user.is_authenticated)
+            return bool(request.user and request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser))
         return False
--- account/viewsets.py
+++ account/viewsets.py
@@ -23,11 +23,6 @@ class UserViewSet(ModelViewSet):
     permission_classes = (AuthenticatedUserPermission,)
     serializer_class = UserSerializer
 
-    def get_queryset(self):
-        if self.request.user.is_superuser:
-            return User.objects.all()
-        return User.objects.exclude(is_superuser=True)
-
     def get_object(self):
         obj = User.objects.get_object_by_public_id(self.kwargs["pk"])
         self.check_object_permissions(self.request, obj)
